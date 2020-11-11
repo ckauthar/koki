023ef2c1a9aaa2713d4771b4896527e132822201
ckauthar <60842577+ckauthar@users.noreply.github.com>
2020-11-11 16:48:20 +0200

    Add files via upload

---
+++ JavaScript/Get HTML Elements/index.js
@@ -0,0 +1,10 @@
+console.log('Script Loaded');
+
+//console.log(document.getElementById("topbar"));
+
+//console.log(document.getElementsByClassName('blog-card'));
+
+//console.log(document.getElementsByTagName('h3'));
+
+console.log(document.querySelectorAll('.blog-card img'));
+console.log(document.querySelector('.blog-card img'));
