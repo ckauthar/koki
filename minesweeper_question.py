4032a550fd83c116cfbe6926cc9dbd4a5466c2d8
ckauthar <60842577+ckauthar@users.noreply.github.com>
2021-03-05 21:54:32 +0200

    Update minesweeper_question.py

--- minesweeper_question.py
+++ minesweeper_question.py
@@ -1,38 +1,51 @@
-# You may create additional functions here:
-board = ["-","-","-","-",
-         "-","-","-","-",
-         "-","-","-","-",
-         "-","-","-","-"]
-
-game_still_going = True
-
-winner = None
-
-current_player = "X"
-
-def display_board():
-  print(board[0] + " | " + board[1] + " | " + board[2] + " | " + board[3])   
-  print(board[4] + " | " + board[5] + " | " + board[6] + " | " + board[7])
-  print(board[8] + " | " + board[9] + " | " + board[10] + " | " + board[11])
-  print(board[12] + " | " + board[13] + " | " + board[14] + " | " + board[15])
-
-
-# Additional Functions above this comment
-
-# Implement your Minesweeper Solution Below:
-
-def play_game():
-    
-    display_board()
-    #Edit the code Below Here
-
-
-
-    pass
-    #Edit the code Above Here
-#play Function Ends Here
-
-
-if __name__=='__main__':
-    play()
-    
+#from random import randint
+import random
+#print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+​
+def game(n, b):
+    arr = [[0 for row in range(n)] for column in range(n)]
+    for num in range(b):
+        x = random.randint(0,n-1)
+        y = random.randint(0,n-1)
+        arr[y][x] = 'X'
+        
+        if (x >=0 and x <= 3) and (y >= 0 and y <= 4):
+            if arr[y][x+1] != 'X':
+                arr[y][x+1] += 1 # center right
+                
+        if (x >=1 and x <= 4) and (y >= 0 and y <= 4):
+            if arr[y][x-1] != 'X':
+                arr[y][x-1] += 1 # center left
+                
+        if (x >= 1 and x <= n-1) and (y >= 1 and y <= n-1):
+            if arr[y-1][x-1] != 'X':
+                arr[y-1][x-1] += 1 # top left
+ 
+        if (x >= 0 and x <= n-2) and (y >= 1 and y <= n-1):
+            if arr[y-1][x+1] != 'X':
+                arr[y-1][x+1] += 1 # top right
+                
+        if (x >= 0 and x <= n-1) and (y >= 1 and y <= n-1):
+            if arr[y-1][x] != 'X':
+                arr[y-1][x] += 1 # top center
+ 
+        if (x >=0 and x <= n-2) and (y >= 0 and y <= n-2):
+            if arr[y+1][x+1] != 'X':
+                arr[y+1][x+1] += 1 # bottom right
+                
+        if (x >= 1 and x <= n-1) and (y >= 0 and y <= n-2):
+            if arr[y+1][x-1] != 'X':
+                arr[y+1][x-1] += 1 # bottom left
+                
+        if (x >= 0 and x <= n-1) and (y >= 0 and y <= n-2):
+            if arr[y+1][x] != 'X':
+                arr[y+1][x] += 1 # bottom center
+                
+    for row in arr:
+        print("\t".join(str(cell) for cell in row))
+        print("  ")
+         
+if __name__ == "__main__":
+    game(5, 6) # Level 1
+    game(6, 8) # level 2
+    game(8, 10) # level 3 
